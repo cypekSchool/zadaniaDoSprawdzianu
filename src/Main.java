3898cd42ffbea83069e1a5ade4daf2250f7aa76f
cypekSchool <mojcyprian9@gmail.com>
2024-09-20 09:51:57 +0200

    upload all src files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        fibonacciExercise();
+        numbersSegregation();
+    }
+
+    public static void fibonacciExercise() {
+        int[] fibonacci = new int[40];
+
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        for (int element : fibonacci) {
+            System.out.println(element + " ");
+        }
+    }
+
+    public static void numbersSegregation() {
+        int[] randomNumbers = new int[100];
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = (int)(Math.random() * 100);
+        }
+
+        for (int number : randomNumbers) {
+            if (number % 2 == 0) evenNumbers.add(number);
+            else                 oddNumbers.add(number);
+        }
+
+        System.out.println("Ilość parzystych: " + evenNumbers.size());
+        System.out.println(evenNumbers);
+        System.out.println("Ilość nieparzystych: " + oddNumbers.size());
+        System.out.println(oddNumbers);
     }
 }
--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class fibonacci {
+public class fibonacci {
+
 }
